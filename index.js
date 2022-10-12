f546ec744dc44f8bf38f815498140acfc6ad2647
Mateo Mansilla <mateobariloche28@gmail.com>
2022-10-12 17:27:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// importing packages 
+const express = require('express');
+const mongoose = require('mongoose');
+const adminRouter = require('./routes/admin');
+
+// importing from files
+const authRouter = require('./routes/auth');
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
+
+// init 
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://MateoMaster:MateoMaster@cluster0.4wggvc5.mongodb.net/?retryWrites=true&w=majority"
+
+// middleware
+app.use(express.json())
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+
+// connection to mongoDB
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log('connection successful');
+    })
+    .catch((e) => {
+        console.log(e); 
+    });
+
+
+app.listen(PORT, ()=> {
+    console.log(`connected at port ${PORT}`);
+});
