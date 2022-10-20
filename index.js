f13786636919fe7a8ae59fd8dc0a046bc6232e0c
Mateo Mansilla <mateobariloche28@gmail.com>
2022-10-20 13:52:04 -0300

    CORS

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.use(productRouter);
 app.use(userRouter);
 app.use(transactionRouter);
 
+app.all('*', function(req, res, next){
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", 'X-Requested-With');
+    next();
+});
+
 
 // connection to mongoDB
 mongoose
