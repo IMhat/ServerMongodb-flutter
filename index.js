b5a57c7d907146f6a9e5261ae784d6c681883d66
Mateo Mansilla <mateobariloche28@gmail.com>
2022-10-27 11:31:24 -0300

    cors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.all('*', function(req, res, next){
 
 app.use(cors({
 // origin: 'https://vocal-pastelito-f96590.netlify.app', 
-origin: 'http://localhost:64755/'  
+origin: 'http://localhost:51098/'  
 }))
 
 // middleware
